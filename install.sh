65c846a1a9942baaff293e18241cf4f32baf10c9
Sebastian Ocampo <sebasop2031@gmail.com>
2026-03-08 15:57:25 -0500

    Added gitignore, added single install file

--- install.sh
+++ install.sh
@@ -6,8 +6,20 @@ sudo apt-get upgrade -y
 
 # Install system dependencies
 sudo apt-get install -y python3 python3-pip python3-venv
+    
+# Create a virtual environment
+python3 -m venv venv
+#activate the virtual environment
+source venv/bin/activate
 
 # Install Python requirements
 pip3 install -r requirements.txt
 
 echo "All dependencies for Raspberry Pi have been installed."
+
+clear 
+
+echo "================================"
+echo "Running cam.py"
+echo "================================"
+python cam.py
